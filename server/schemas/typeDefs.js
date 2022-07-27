99da9699619c085f57cec4d774e93cabf25491f4
hkagei <k_dawg_1022@hotmail.com>
2022-07-27 09:41:59 -0600

    21.2 done

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,6 +31,7 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
@@ -40,6 +41,9 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 
   type Auth {

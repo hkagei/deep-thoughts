78088dad32804e12088c137c819b1877a7fe4687
hkagei <k_dawg_1022@hotmail.com>
2022-08-05 17:17:33 -0600

    finish 21.3

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,10 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+// ApolloProvider is a special type of React component that we'll use to provide data to all of the other components.
+// ApolloClient is a constructor function that will help initialize the connection to the GraphQL API server.
+// InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
+// createHttpLink allows us to control how the Apollo Client makes a request. Think of it like middleware for the outbound network requests.
+
 import React from 'react';
 
 import Header from './components/Header';
@@ -5,15 +12,28 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// the consts above establish the connection to the back-end server's /graphql endpoint.
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 

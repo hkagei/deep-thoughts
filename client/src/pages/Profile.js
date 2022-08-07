67ac65fd0e067b36ec12342d8d019ce9d4eae63c
hkagei <k_dawg_1022@hotmail.com>
2022-08-07 11:21:36 -0600

    finish 21.6.4

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,18 +3,33 @@ import { Navigate, useParams } from "react-router-dom";
 
 import ThoughtList from "../components/ThoughtList";
 
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
+
+import ThoughtForm from '../components/ThoughtForm';
+
+import { ADD_FRIEND } from "../utils/mutations";
 
 import FriendList from "../components/FriendList";
 import Auth from "../utils/auth";
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
   const user = data?.me || data?.user || {};
   // navigate to personal profile page if username is the logged-in user's
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -24,11 +39,12 @@ const Profile = () => {
   if (loading) {
     return <div>Loading...</div>;
   }
-  
+
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
       </h4>
     );
   }
@@ -39,6 +55,12 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
